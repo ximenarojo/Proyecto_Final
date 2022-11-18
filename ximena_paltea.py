051cfd56d5cc37bbd0eecf6eea5769de5f243d42
RodriCueva <112134053+RodriCueva@users.noreply.github.com>
2022-11-18 09:01:37 -0500

    Update ximena_paltea.py

--- ximena_paltea.py
+++ ximena_paltea.py
@@ -4,9 +4,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import gdown
-import os
 from opencage.geocoder import OpenCageGeocode
 
 DataUbigeos = pd.read_csv('ubigeos-completos.csv')
