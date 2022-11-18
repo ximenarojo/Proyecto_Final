54793f8db20402f050e690ba89a83ec7e660831c
RodriCueva <112134053+RodriCueva@users.noreply.github.com>
2022-11-17 23:22:06 -0500

    Update prueba_3_jejej.py

--- prueba_3_jejej.py
+++ prueba_3_jejej.py
@@ -19,7 +19,7 @@ st.markdown("""
 	   adquiridos en las clases teóricas y prácticas durante del ciclo, junto a la asesoría y enseñansas de nuestros profesores, Fonse <3.
 	   """) 
 image = Image.open('tenorio el papi.jpg')
-st.image(image, caption='Piura: Gobierno regional pone a disposición información hidrometeorológica del sistema hidráulico Chira - Piura', use_column_width=True)
+st.image(image, caption='Futuros Ingenieros Ambientales', use_column_width=True)
 
 st.header('Agua y saneamiento')
 st.markdown("""
