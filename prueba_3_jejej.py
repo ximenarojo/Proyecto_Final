4c80a7246260f5df25c1a7aacd6fd87ba4d9fe2e
RodriCueva <112134053+RodriCueva@users.noreply.github.com>
2022-11-17 21:29:23 -0500

    Update prueba_3_jejej.py

--- prueba_3_jejej.py
+++ prueba_3_jejej.py
@@ -19,9 +19,8 @@ st.markdown("""
 	de las presas a las 07:00 y precipitaciones.La cuenca es una extensión de terreno en un valle, escurren aguas formando un río atravesando valles 
 	y escurriendo en el mar. Una cuenca puede tener varias estaciones hidrometeorológicas.El dato de precipitación es la lluvia acumulada entre las 
 	7:00 horas del día anterior y las 7:00 horas de hoy (24 horas), cuando se considera el campo vacío, indica que no se realizaron mediciones.
-	* **###Para mayor información también puede ingresar a:** http://servicios.regionpiura.gob.pe/datosh
-                                                                  https://www.datosabiertos.gob.pe/dataset/datos-hidrometereol%C3%B3gicos-gobierno-regional-piura"""
-
+	* **###Para mayor información también puede ingresar a:** http://servicios.regionpiura.gob.pe/datosh""")
+from PIL import Image
 image = Image.open('Proyecto_Piura.jpg')
 st.image(image, caption='La contaminación por el parque automotor antiguo es un problema en Lima Metropolitana', use_column_width=True)
 
