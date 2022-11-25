abdf6dc78ca48485c637120b30e0bceb4bfc6faa
RodriCueva <112134053+RodriCueva@users.noreply.github.com>
2022-11-25 11:03:16 -0500

    Update boken_para_proyecto.py

--- boken_para_proyecto.py
+++ boken_para_proyecto.py
@@ -8,16 +8,6 @@ df_visualizacion=pd.read_csv('TB_UBIGEOS.csv')
 
 st.dataframe(df_visualizacion)
 
-df_actividad_freq = pd.DataFrame(df_visualizacion["provincia"].value_counts())
-labels = df_actividad_freq.index.tolist()
-sizes = df_actividad_freq["provincia"].tolist()
-fig1, ax1 = plt.subplots()
-ax1.pie(sizes, labels=labels, autopct='%1.1f%%',
-startangle=0)
-#plt.title('Distribucion de datos segun provincia')
-ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-st.pyplot(fig1)
-
 df_anho_freq = pd.DataFrame(df_visualizacion["region"].value_counts())
 st.bar_chart(df_anho_freq)
 
