72ba4ad3788d7dbc81e79510d463cdeea115e4e8
RodriCueva <112134053+RodriCueva@users.noreply.github.com>
2022-11-25 09:24:40 -0500

    Update boken_para_proyecto.py

--- boken_para_proyecto.py
+++ boken_para_proyecto.py
@@ -1,12 +1,14 @@
 import streamlit as st
-from bokeh.plotting import figure
+import pandas as pd
+import numpy as np
 
+@st.experimental_memo
+def load_data():
+    df = pd.DataFrame(
+        np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4], columns=["lat", "lon"]
+    )
+    return df
 
-x = [1, 2, 3, 4, 5]
-y = [6, 7, 2, 4, 5]
+df = load_data()
 
-p = figure(title="simple line example", x_axis_label="x", y_axis_label="y")
-
-p.line(x, y, legend_label="Trend", line_width=2)
-
-st.bokeh_chart(p)
+st.map(df)
