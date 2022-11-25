045aecd04a3d39616dce5ca21b51f446950c2971
RodriCueva <112134053+RodriCueva@users.noreply.github.com>
2022-11-25 10:43:06 -0500

    Update boken_para_proyecto.py

--- boken_para_proyecto.py
+++ boken_para_proyecto.py
@@ -8,3 +8,7 @@ df_visualizacion=pd.read_csv('TB_UBIGEOS.csv')
 
 st.dataframe(df_visualizacion)
 
+df_visualizacion = df_visualizacion.rename(columns={'latitud':'LAT', 'longitud':'LON'})
+st.map(df_visualizacion[['LAT','LON']])
+
+
