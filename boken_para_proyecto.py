e9cac1b92c0aa7bf3e6fd2c42f6774214dfc18b6
RodriCueva <112134053+RodriCueva@users.noreply.github.com>
2022-11-25 10:38:50 -0500

    Update boken_para_proyecto.py

--- boken_para_proyecto.py
+++ boken_para_proyecto.py
@@ -4,11 +4,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+df_visualizacion=pd.read_csv('TB_UBIGEOS.csv')
 
-
-df_ubigeo=pd.read_csv('TB_UBIGEOS.csv')
-
-
-st.map(df_ubigeo)
-
+df_visualizacion = df_visualizacion.rename(columns={'latitud':'lat', 'longitud':'lon'})
+st.map(df_visualizacion[['lat','lon']])
 
