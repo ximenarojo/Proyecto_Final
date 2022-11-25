47a3befc96699dd02a3bc8ee4892c227bdc9bf9d
RodriCueva <112134053+RodriCueva@users.noreply.github.com>
2022-11-25 10:53:18 -0500

    Update boken_para_proyecto.py

--- boken_para_proyecto.py
+++ boken_para_proyecto.py
@@ -8,6 +8,9 @@ df_visualizacion=pd.read_csv('TB_UBIGEOS.csv')
 
 st.dataframe(df_visualizacion)
 
+df_anho_freq = pd.DataFrame(df_visualizacion["region"].value_counts())
+st.bar_chart(df_anho_freq)
+
 df_visualizacion = df_visualizacion.rename(columns={'latitud':'LAT', 'longitud':'LON'})
 st.map(df_visualizacion[['LAT','LON']])
 
