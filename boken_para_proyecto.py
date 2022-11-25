e4303d2fe01258969dd1884db706b4c9f39065ce
RodriCueva <112134053+RodriCueva@users.noreply.github.com>
2022-11-25 10:06:19 -0500

    Update boken_para_proyecto.py

--- boken_para_proyecto.py
+++ boken_para_proyecto.py
@@ -7,12 +7,8 @@ df_mapa = pd.read_csv('DATOS_HIDROMETEREOLOGICOS_GORE_PIURA_2.csv')
 df_ubigeo = pd.read_csv('TB_UBIGEOS.csv')
 
 @st.experimental_memo
-def load_data():
-    df_ubigeo= df_ubigeo.rename(columns={'LATITUD':'lat', 'LONGITUD':'lon'})
-    st.map(df_ubigeo[['lat','lon']])
-    
-    return df_mapa
 
-c = load_data()
+df_ubigeo = df_ubigeo.rename(columns={'LATITUD':'lat', 'LONGITUD':'lon'})
+st.map(df_ubigeo[['lat','lon']])
 
-st.map(c)
+
