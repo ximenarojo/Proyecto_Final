70123bd81a82e5f1c3818bb97a3bc22b30de5b71
RodriCueva <112134053+RodriCueva@users.noreply.github.com>
2022-11-25 10:28:52 -0500

    Update boken_para_proyecto.py

--- boken_para_proyecto.py
+++ boken_para_proyecto.py
@@ -4,12 +4,12 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-df_mapa = pd.read_csv('DATOS_HIDROMETEREOLOGICOS_GORE_PIURA_2.csv')
+
 
 df_ubigeo = pd.read_csv('TB_UBIGEOS.csv')
 
 
-df = df.rename(columns={'latitud':'lat', 'longitud':'lon'})
-st.map(df[['lat','lon']])
+df_ubigeo=df_ubigeo.rename(columns={'latitud':'lat', 'longitud':'lon'})
+st.map(df_ubigeo[['lat','lon']])
 
 
