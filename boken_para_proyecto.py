26a6de862f1c05ba172b311f003eaf367da4e1ba
RodriCueva <112134053+RodriCueva@users.noreply.github.com>
2022-11-25 10:19:36 -0500

    Update boken_para_proyecto.py

--- boken_para_proyecto.py
+++ boken_para_proyecto.py
@@ -10,7 +10,7 @@ df_ubigeo = pd.read_csv('TB_UBIGEOS.csv')
 
 @st.experimental_memo
 
-df_ubigeo = df_ubigeo.rename(columns={'LATITUD':'latitud', 'LONGITUD':'longitud'})
-st.map(df_ubigeo[['latitud','longitud']])
+df_ubigeo = df_ubigeo.rename(columns={'latitud':'lat', 'longitud':'lon'})
+st.map(df_ubigeo[['lat','lon']])
 
 
