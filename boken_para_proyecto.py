3987c36545cfec981b9bbb9b105356bcc409cb6f
RodriCueva <112134053+RodriCueva@users.noreply.github.com>
2022-11-26 00:12:12 -0500

    Update boken_para_proyecto.py

--- boken_para_proyecto.py
+++ boken_para_proyecto.py
@@ -12,7 +12,7 @@ df_anho_freq = pd.DataFrame(df_visualizacion["region"].value_counts())
 st.bar_chart(df_anho_freq)
 
 
-df =  df_visualizacion.rename(columns={'LATITUD':'lat', 'LONGITUD':'lon'})
+df =  df_visualizacion.rename(columns={'latitud':'lat', 'longitud':'lon'})
 
 st.map(df)
 
