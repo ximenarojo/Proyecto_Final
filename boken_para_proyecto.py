16735b38b11543b76e6cffc1b81e808adbb4ca06
RodriCueva <112134053+RodriCueva@users.noreply.github.com>
2022-11-26 00:09:49 -0500

    Update boken_para_proyecto.py

--- boken_para_proyecto.py
+++ boken_para_proyecto.py
@@ -4,14 +4,16 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-df_visualizacion=pd.read_csv('TB_UBIGEOS.csv')
+df_mapa=pd.read_csv('TB_UBIGEOS.csv')
 
 st.dataframe(df_visualizacion)
 
 df_anho_freq = pd.DataFrame(df_visualizacion["region"].value_counts())
 st.bar_chart(df_anho_freq)
 
-df_visualizacion = df_visualizacion.rename(columns={'latitud':'lat', 'longitud':'lon'})
-st.map(df_visualizacion[['lat','lon']])
+
+df =  df_mapa.rename(columns={'LATITUD':'lat', 'LONGITUD':'lon'})
+
+st.map(df)
 
 
