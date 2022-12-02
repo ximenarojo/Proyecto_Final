6cce94d8f2e4fb221c45f223afb1a9b79640e7c4
RodriCueva <112134053+RodriCueva@users.noreply.github.com>
2022-12-02 02:06:15 -0500

    Update codigo_app.py

--- codigo_app.py
+++ codigo_app.py
@@ -144,9 +144,9 @@ t1 = '• Frecuencia de los proyectos '+estado+' según la clasificación ACTIVI
 st.subheader(t1)
 source = pd.DataFrame({"category": [1, 2, 3, 4, 5, 6], "value": [4, 6, 10, 3, 7, 8]})
 
-alt.Chart(source).mark_arc(innerRadius=50).encode(
+alt.Chart(source).mark_arc().encode(
     theta=alt.Theta(field="value", type="quantitative"),
-    color=alt.Color(field="DISTRITO", type="nominal"),
+    color=alt.Color(field="category", type="nominal"),
 )
 
 	
